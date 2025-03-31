ae152af1de03ac643f46b6c6986aae5a6f880e49
Alejandro Martínez <amj.parrales@gmail.com>
2025-03-31 13:16:36 +0200

    Singleton de MindStone

---
+++ src/main/java/org/microservicios1/Singletons/MindStoneSingleton.java
@@ -0,0 +1,27 @@
+package org.microservicios1.Singletons;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.java.Log;
+import org.microservicios1.models.MindStone;
+
+@Log
+@NoArgsConstructor (access = AccessLevel.PRIVATE)
+public class MindStoneSingleton {
+
+    private static volatile MindStone mindStoneInstance;
+
+    //Metodo para acceder a la instancia.
+    public static MindStone getInstance(){
+        if (mindStoneInstance == null){
+            log.info("Creating first instance");
+            synchronized (MindStoneSingleton.class){
+                if (mindStoneInstance == null){
+                    mindStoneInstance = new MindStone();
+                }
+            }
+        }
+        return mindStoneInstance;
+    }
+
+}
