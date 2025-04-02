095cf96cb61e596a3ac50f1f6cf36121063916f0
Alejandro Martínez <amj.parrales@gmail.com>
2025-04-02 11:10:19 +0200

    Factory de Space

--- src/main/java/org/microservicios1/Factories/SpaceStoneFactory.java
+++ src/main/java/org/microservicios1/Factories/SpaceStoneFactory.java
@@ -1,23 +1,23 @@
 package org.microservicios1.Factories;
 
 import org.microservicios1.Prototype.Prototypes;
-import org.microservicios1.Singletons.MindStoneSingleton;
-import org.microservicios1.models.MindStone;
+import org.microservicios1.Singletons.SpaceStoneSingleton;
+import org.microservicios1.models.SpaceStone;
 import org.microservicios1.models.Stone;
 
-public class MindStoneFactory extends ConfigurableStoneFactory{
+public class SpaceStoneFactory extends ConfigurableStoneFactory{
+
     @Override
     public Stone createStone() {
 
         // Verifica si la configuración indica que debe ser Singleton.
         if (super.isSingleton()){
-
-            // Si es Singleton, devuelve la única instancia de MindStone.
-            return MindStoneSingleton.getInstance();
+            // Si es Singleton, devuelve la única instancia de SpaceStone.
+            return SpaceStoneSingleton.getInstance();
 
         }else {
             // Si no es Singleton, crea una nueva instancia utilizando el Prototype.
-            return Prototypes.mindPrototype.buildPrototype(new MindStone());
+            return Prototypes.spacePrototype.buildPrototype(new SpaceStone());
         }
     }
 }
